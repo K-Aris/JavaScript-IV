bbef6a1d46679c89f02f1fc54282ee7a3f9b062b
Kurt Aris <kurtarisohn@gmail.com>
2019-10-10 21:31:55 -0700

    Completed lambda-classes.j

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,65 +1,143 @@
-// // CODE here for your Lambda Classes
+// Lambda School 
 
-// * We have a school to build here! This project will get you used to thinking about classes in JavaScript and building them from a brand new data set.
-// * Lambda personnel can be broken down into three different types of `people`.
-//   * **Instructors** - extensions of Person
-//   * **Students** - extensions of Person
-//   * **Project Managers** - extensions of Instructors
-// * **IMPORTANT** - You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes. For example:
+class Person {
+    constructor(personProp) {
+        this.firstName = personProp.firstName; 
+        this.lastName = personProp.lastName; 
+        this.age = personProp.age; 
+        this.location = personProp.location; 
+    }
+    speak() {
+        return `Hello my name is ${this.firstName} ${this.lastName}, I'm from ${this.location}`; 
+    }
+}
 
-// ```js
-// const fred = new Instructor({
-//   name: 'Fred',
-//   location: 'Bedrock',
-//   age: 37,
-//   favLanguage: 'JavaScript',
-//   specialty: 'Front-end',
-//   catchPhrase: `Don't forget the homies`
-// });
-// ```
+class Instructor extends Person {
+    constructor(instProp) {
+        super(instProp);
+        this.goodAt = instProp.goodAt;
+        this.favLang = instProp.favLang;
+        this.motto = instProp.motto;
+        }
+        demo(subject) {
+            return `Today we are learning about ${subject}`;
+        }
+        grade(student, subject) {
+            return `${student.firstName} receives a perfect score on ${subject}`; 
+        }
+}
 
-// #### Person
 
-// * First we need a Person class. This will be our `base-class`
-// * Person receives `name` `age` `location` all as props
-// * Person receives `speak` as a method.
-// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
+class Student extends Person {
+    constructor(studentProp) {
+        super(studentProp);
+        this.previousBackground = studentProp.previousBackground; // what they did before
+        this.className = studentProp.className; //PT 11
+        this.favSubj = studentProp.favSubj; //an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript'
+    }
+    //   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+    listSubjects() {
+        return `${this.firstName} likes ${this.favSubj}`; 
+        // return - favSubj logged out one by one 
+    }
 
-// #### Instructor
+    PRAssignment(subject) {
+        return `${this.firstName} has submitted a PR for: ${subject}`;
+    }
 
-// * Now that we have a Person as our base class, we'll build our Instructor class.
-// * Instructor uses the same attributes that have been set up by Person
-// * Instructor has the following unique props:
-//   * `specialty` what the Instructor is good at i.e. 'redux'
-//   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-//   * `catchPhrase` i.e. `Don't forget the homies`
-// * Instructor has the following methods:
-//   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-//   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+    sprintChallenge(subject) {
+        return `${this.firstName} has begun sprint challenge on ${subject}`;
+    }
+}
 
-// #### Student
 
-// * Now we need some students!
-// * Student uses the same attributes that have been set up by Person
-// * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-// * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+class ProjectManagers extends Instructor {
+    constructor(pmProp){
+        super(pmProp);  
+        this.gradClassName = pmProp.gradClassName;
+        this.favInstructor = pmProp.favInstructor;
+    }
+    standUp(slackChannel) {
+        return `${this.firstName} announces to ${slackChannel}, @channel standy times!`;
+    }
 
-// #### Project Manager
+    debugCode(student, subject) {
+        return `${this.firstName} debugs ${student.firstName}'s code on ${subject}`; 
+    }
+}
 
-// * Now that we have instructors and students, we'd be nowhere without our PM's
-// * ProjectManagers are extensions of Instructors
-// * ProjectManagers have the following unique props:
-//   * `gradClassName`: i.e. CS1
-//   * `favInstructor`: i.e. Sean
-// * ProjectManagers have the following Methods:
-//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
+
+const amy = new Person({
+    firstName: "Amy",
+    lastName: "Another",
+    age: 25,
+    location: "Canada", 
+})
+
+
+const fred = new Instructor({
+    firstName: 'Fred',
+    lastName: 'Jones',
+    location: 'Bedrock',
+    age: 37,
+    favLang: 'JavaScript',
+    goodAt: 'Front-end',
+    motto: "Don't forget the homies", 
+});
+
+
+const chris = new Student ({
+    firstName: 'Chis',
+    lastName: 'Hernz',
+    location: 'Cali',
+    age: 37,
+    previousBackground: "Pilates Instructor",
+    className: "WebPt11",
+    favSubj: "cobal"
+});
+
+const bill  = new ProjectManagers({
+    firstName: 'Bill',
+    lastName: 'Gates',
+    location: 'Microsoft',
+    age: 92,
+    favLang: 'Wine',
+    goodAt: 'Being A Billionaire',
+    motto: "We be curing polio",
+    gradClassName: "DOS for Dummies",
+    favInstructor: "Steve Jobs",
+});
+
+//Person
+console.log(amy);
+
+//Instructor
+console.log(fred);
+console.log(fred.speak()); 
+console.log(fred.demo("CSS"));
+console.log(fred.grade(chris, "Theoretical Physics"));
+
+//Student 
+console.log(chris);
+console.log(chris.listSubjects());
+console.log(chris.PRAssignment("super computing"));
+console.log(chris.sprintChallenge("AI"));
+
+//ProjectManagers
+console.log(bill);
+console.log(bill.standUp("Code4Life"));
+console.log(bill.debugCode(chris, "AS400")); 
+
+
+//Constructor Check 
+console.log(Person);
+console.log(Student);
+console.log(Instructor);
+console.log(ProjectManagers);
+
+
+
+
 
 // #### Stretch Problem
 
@@ -68,4 +146,3 @@
 // * Add a graduate method to a student.
 //   * This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
 //   * If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
-
